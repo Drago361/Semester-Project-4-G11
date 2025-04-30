a5995ba281626338ef1bef0b5a64d14056784fe3
AlexFTV <144455302+Drago361@users.noreply.github.com>
2025-04-30 16:11:09 +0200

    Added the base for the project
    Added the front-end, still working on the JS part of it and the addition of the newly created parts of the front-end.

---
+++ SPv1/base/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, "base/index.html")
+
+def index(request):
+    return render(request, "base/index.html")
+
+def faq(request):
+    return render(request, "base/faq.html")
+
+def terms(request):
+    return render(request, "base/terms.html")
+
+def privacy(request):
+    return render(request, "base/privacy.html")
+
+def placeholder(request):
+    return render(HttpResponse("This is a placeholder view."))
