34fe0f687be01d73898f53db44ed86bad7d31fb9
AlexFTV <144455302+Drago361@users.noreply.github.com>
2025-05-27 20:04:21 +0200

    database work

--- SPv1/base/admin.py
+++ SPv1/base/admin.py
@@ -2,11 +2,28 @@ from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 from base.models import Book
+from import_export import resources
+
+def before_import(self, dataset, **kwargs):
+    super().before_import(dataset, **kwargs)
+    dataset.append_col([None] * len(dataset), header='id')
 
 class BookResource(resources.ModelResource):
 
     class Meta:
         model = Book  # or 'core.Book'
+        import_id_fields = ('asin',)
+        fields = (
+            'asin', 'title', 'author', 'soldBy', 'imgUrl', 'productUrl',
+            'stars', 'reviews', 'price', 'isKindleUnlimited',
+            'categoryId', 'isBestSeller', 'isEditorsPick',
+            'isGoodReadsChoice', 'publishedDate', 'categoryName')
+
+def before_import(self, dataset, **kwargs):
+    super().before_import(dataset, **kwargs)
+    dataset.append_col([None] * len(dataset), header='id')
+
+
 
 @admin.register(Book)
 class BookAdmin(ImportExportModelAdmin):
--- SPv1/base/models.py
+++ SPv1/base/models.py
@@ -1,4 +1,37 @@
 from django.db import models
+from import_export import resources
+
+class Book(models.Model):
+    asin = models.CharField(max_length=20, primary_key=True, null=False, blank=False)
+    # ASIN: Amazon Standard Identification Number
+    title = models.TextField(null=False, blank=False)
+    # Title of the book
+    author = models.CharField(max_length=255, null=False, blank=False)
+    # Author(s) of the book
+    soldBy = models.CharField(max_length=255, null=True, blank=True)
+    # Seller of the book
+    imgUrl = models.URLField()
+    # URL of the book's image
+    productUrl = models.URLField(null=True, blank=True)
+    # URL of the book's product page
+    stars = models.DecimalField(max_digits=3, decimal_places=2)
+    reviews = models.IntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    isKindleUnlimited = models.BooleanField(null=True, blank=True)
+    categoryId = models.IntegerField(null=True, blank=True)
+    # Category ID of the book
+    isBestSeller = models.BooleanField()
+    isEditorsPick = models.BooleanField()
+    isGoodReadsChoice = models.BooleanField()
+    publishedDate = models.DateField(  null=True, blank=True)
+    categoryName = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+    
+    def before_import(self, dataset, **kwargs):
+       super().before_import(dataset, **kwargs)
+       dataset.append_col([], header='id')
 
 '''class Book(models.Model):
     name = models.CharField(max_length=100)
@@ -10,7 +43,7 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()'''
 
-class Book(models.Model):
+'''class Book(models.Model):
     bookID = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     authors = models.CharField(max_length=200)
@@ -23,7 +56,7 @@ class Book(models.Model):
     text_reviews_count = models.IntegerField()
     
     def __str__(self):
-        return self.name
+        return self.name'''
     
 '''class Author(models.Model):
     name = models.CharField(max_length=100)
